9dd1d39ea1e165e4896d701d20efe382f1fd7ec9
Brian <brian.pesky041@passmail.com>
2026-09-24 22:03:09 -0400

    Completed exercises on Execute Python Scripts lesson

---
+++ python-basics/practice.py
@@ -0,0 +1,20 @@
+# https://pythonbasics.org/execute-python-scripts/
+
+def name():
+    print("My name is Brian")
+
+def annoyingSong():
+    print("I know a song that gets on everybody's nerves")
+    print("Everybody's nerves")
+    print("Everybody's nerves")
+    print("I know a song that gets on everybody's nerves")
+    print("And this is how it goes...")
+
+def main():
+    #name()
+    #annoyingSong()
+
+
+    print("End of main func")
+
+main()

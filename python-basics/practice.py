43ca936defd0ffa0613ef2fbd1b7901253755867
Brian <brian.pesky041@passmail.com>
2026-09-24 22:16:15 -0400

    4 more exercises completed

--- python-basics/practice.py
+++ python-basics/practice.py
@@ -10,10 +10,39 @@ def annoyingSong():
     print("I know a song that gets on everybody's nerves")
     print("And this is how it goes...")
 
-def main():
-    #name()
-    #annoyingSong()
+#https://pythonbasics.org/variables/
+def printNums():
+    num1 = 10 
+    num2 = 4.4
+    num3 = 3.14159
+    print("Integer: ", num1)
+    print("Float: ", num2)
+    print("Pi: ", num3)
 
+def add():
+    print("64 + 32 =", 64 + 32)
+
+def addTwoNums(x,y):
+    print(x, "+", y, "=", x+y)
+
+# https://pythonbasics.org/strings/
+def actorName():
+    print("Bryan Cranston")
+
+def lucky():
+    # alternative method of combining numbers and text
+    s = "My lucky number is " + str(7) + ", what is yours?"
+    print(s)
+    print(s[3:8])
+
+def main():
+    # name()
+    # annoyingSong()
+    # printNums()
+    # add()
+    # addTwoNums(30, 100)
+    # actorName()
+    # lucky()
 
     print("End of main func")
 
